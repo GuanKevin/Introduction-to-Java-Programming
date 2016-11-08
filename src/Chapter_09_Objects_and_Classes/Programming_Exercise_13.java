bbb65a88d62ee99e3e4ac583fe0bef80354788fe
Kevin Guan <Kevin Guan>
2016-11-07 20:27:14 -0500

    Completed CHP09 PE13

--- src/Chapter_09_Objects_and_Classes/Programming_Exercise_13.java
+++ src/Chapter_09_Objects_and_Classes/Programming_Exercise_13.java
@@ -1,17 +1,19 @@
 package Chapter_09_Objects_and_Classes;
+
 import java.util.Random;
 import java.util.Scanner;
 
 /**
  * The Location class
  * Design a class named Location for locating a maximal value and its location in a two-dimensional array. 
- * The class contains public data fields row, column, and maxValue that store the maximal value and its indices in a two-dimensional array with row and column as int types and maxValue as a double type. 
+ * The class contains public data fields row, column, and maxValue that store the maximal value 
+ * and its indices in a two-dimensional array with row and column as int types and maxValue as a double type. 
  * Write the following method that returns the location of the largest element in a two-dimensional array:
  * public static Location locateLargest(double[][] a)
  * The return value is an instance of Location. 
  * Write a test program that prompts the user to enter a two-dimensional array and displays the location of the largest element in the array.
  * 
- * 03/01/2016
+ * 11/07/2016
  * @author kevgu
  *
  */
@@ -22,57 +24,56 @@ public class Programming_Exercise_13
 	{
 		Scanner input = new Scanner(System.in);
 		
-		System.out.print("Enter the number of rows and columns in the array: ");
-		System.out.println("The location of the largest element is value " + Location.maxValue + " at (" + Location.row + ", " + Location.column + ")");
+		System.out.print("Enter row and column of matrix: ");
+		double[][] matrix = generateMatrix(input.nextInt(), input.nextInt());
+		displayMatrix(matrix);
+		Location location = Location.locateLargest(matrix);
+		System.out.println("(" + location.row + "," + location.column + ") " + location.maxValue);
 		
 		input.close();
 	}
-}
-
-class Location 
-{
-	public static int row, column = 0;
-	public static  double maxValue = 0;
-	public static double[][] values = null;
 	
-	Location(int row, int column)
+	public static void displayMatrix(double[][] matrix)
 	{
-		values = new double[row][column];
-		insert();
-		display();
-		search();
-	}
-	
-	public static void insert()
-	{
-		Random randNumber = new Random();
-		
-		for (int i = 0; i < values.length; i++)
-			for (int j = 0; j < values[i].length; j++)
-				values[i][j] = randNumber.nextInt(100); 
-	}
-	
-	public static void display()
-	{
-		Random randNumber = new Random();
-		
-		for (int i = 0; i < values.length; i++)
+		for (int i = 0; i < matrix.length; i++)
 		{
-			for (int j = 0; j < values[i].length; j++)
-				System.out.print(values[i][j] + " ");
+			for (int j = 0; j < matrix[i].length; j++)
+				System.out.printf("%3.1f ", matrix[i][j]);
 			System.out.println();
 		}
 	}
 	
-	public static void search()
+	public static double[][] generateMatrix(int row, int column)
 	{
-		for (int i = 0; i < values.length; i++)
-			for (int j = 0; j < values[i].length; j++)
-				if(values[i][j] > maxValue)
+		Random randomDouble = new Random();
+		double[][] matrix = new double[row][column];
+		
+		for (int i = 0; i < matrix.length; i++)
+			for (int j = 0; j < matrix[i].length; j++)
+				matrix[i][j] = randomDouble.nextDouble() * 10;
+		
+		return matrix;
+	}
+}
+
+class Location
+{
+	int row, column;
+	double maxValue;
+	
+	public static Location locateLargest(double[][] matrix)
+	{
+		Location location = new Location();
+		location.maxValue = 0;
+		
+		for (int i = 0; i < matrix.length; i++)
+			for (int j = 0; j < matrix[i].length; j++)
+				if (location.maxValue < matrix[i][j])
 				{
-					row = i;
-					column = j;
-					maxValue = values[i][j];
+					location.maxValue = matrix[i][j];
+					location.row = i;
+					location.column = j;
 				}
+		return location;
 	}
 }
