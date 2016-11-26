3861ebb0ddbded460331c30c95f8d1f220c32ec6
Kevin Guan <Kevin Guan>
2016-11-25 23:06:03 -0500

    Completed CHP10 PE17

--- src/Chapter_10_Object_Oriented_Thinking/Programming_Exercise_17.java
+++ src/Chapter_10_Object_Oriented_Thinking/Programming_Exercise_17.java
@@ -1,7 +1,6 @@
 package Chapter_10_Object_Oriented_Thinking;
-import java.util.Scanner;
 
-import Utilities.SquareNumbers;
+import java.math.BigDecimal;
 
 /**
  * Square numbers
@@ -9,19 +8,21 @@ import Utilities.SquareNumbers;
  * A square number is a number in the form of n^2. For example, 4, 9, and 16 are square numbers. 
  * Find an efficient approach to run your program fast.
  * 
- * 03/10/2016
+ * 11/25/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_17 
 {
-	Scanner input = new Scanner(System.in);
 	public static void main(String[] args) 
 	{
-		System.out.println(Long.MAX_VALUE);
-		System.out.println("Square numbers greater than Long.Max_Value");
-		SquareNumbers sn = new SquareNumbers();
-		sn.squareNumberGreaterThan(10);
+		BigDecimal bigNum = new BigDecimal(Math.round(Math.sqrt(Long.MAX_VALUE)));
+		
+		for (int i = 1; i <= 10; i++)
+		{
+			System.out.println(bigNum.multiply(bigNum));
+			bigNum = bigNum.add(BigDecimal.ONE);
+		}
 	}
 }
