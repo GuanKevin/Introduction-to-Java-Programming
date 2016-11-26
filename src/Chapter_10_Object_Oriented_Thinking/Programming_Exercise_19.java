bea881a10be6bb998d406a9d9e487b175c0c94ff
Kevin Guan <Kevin Guan>
2016-11-26 13:23:14 -0500

    Completed CHP10 PE19

--- src/Chapter_10_Object_Oriented_Thinking/Programming_Exercise_19.java
+++ src/Chapter_10_Object_Oriented_Thinking/Programming_Exercise_19.java
@@ -8,7 +8,7 @@ import java.math.BigInteger;
  * Write a program that finds all Mersenne primes with p … 100 and displays the output as shown below. 
  * (Hint: You have to use BigInteger to store the number, because it is too big to be stored in long. Your program may take several hours to run.)
  * 
- * 11/10/2016
+ * 11/26/2016
  * @author kevgu
  *
  */
@@ -19,10 +19,10 @@ public class Programming_Exercise_19
 	{
 		BigInteger bigNum = new BigInteger("3");
 		
-		int count = 0;
+		int count = 1;
 		System.out.printf("%s %8s\n", "p", "2^p - 1");
 		
-		while (count < 10)
+		while (count <= 10)
 		{
 			BigInteger mersennePrime = power(new BigInteger("2"), bigNum);
 			if (isMersennePrime(mersennePrime))
@@ -37,12 +37,15 @@ public class Programming_Exercise_19
 	public static BigInteger power(BigInteger base, BigInteger exponent)
 	{
 		BigInteger result = BigInteger.ONE;
-		  while (exponent.signum() > 0) {
-		    if (exponent.testBit(0)) result = result.multiply(base);
-		    base = base.multiply(base);
-		    exponent = exponent.shiftRight(1);
-		  }
-		  return result.subtract(BigInteger.ONE);
+		
+		while (exponent.signum() > 0) 
+		{
+			if (exponent.testBit(0)) result = result.multiply(base);
+			    base = base.multiply(base);
+			    exponent = exponent.shiftRight(1);
+		}
+		  
+		return result.subtract(BigInteger.ONE);
 	}
 	
 	public static boolean isMersennePrime(BigInteger bigNum)
