f65e4273b51e5032d0b5ad9e2b48d55e1d5a7069
Kevin Guan <Kevin Guan>
2016-11-25 22:34:42 -0500

    Completed CHP10 PE16

--- src/Chapter_10_Object_Oriented_Thinking/Programming_Exercise_16.java
+++ src/Chapter_10_Object_Oriented_Thinking/Programming_Exercise_16.java
@@ -1,23 +1,33 @@
 package Chapter_10_Object_Oriented_Thinking;
-import java.util.Scanner;
 
-import Utilities.DivisibleBy;
+import java.math.BigInteger;
 
 /**
  * Divisible by 2 or 3
  * Find the first ten numbers with 50 decimal digits that are divisible by 2 or 3.
  * 
- * 03/10/2016
+ * 11/25/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_16 
 {
-	Scanner input = new Scanner(System.in);
 	public static void main(String[] args) 
 	{
-		DivisibleBy db = new DivisibleBy();
-		db.printDivisibleBy();
+		BigInteger bigNum = new BigInteger("10000000000000000000000000000000000000000000000000");
+		
+		int count = 1;
+		    
+		while (count <= 10) 
+		{
+			if (bigNum.remainder(new BigInteger("2")).equals(BigInteger.ZERO) || bigNum.divide(new BigInteger("3")).equals(BigInteger.ZERO)) 
+			{
+				System.out.println(bigNum);
+		        count++;
+			}
+			
+			bigNum = bigNum.add(BigInteger.ONE);
+		}
 	}
 }
