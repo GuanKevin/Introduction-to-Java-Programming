990a75ca12d5638bd8671b31cacc2ab914d263c1
Kevin Guan <Kevin Guan>
2016-11-26 21:27:51 -0500

    Completed CHP10 PE27

--- src/Chapter_10_Object_Oriented_Thinking/Programming_Exercise_27.java
+++ src/Chapter_10_Object_Oriented_Thinking/Programming_Exercise_27.java
@@ -1,9 +1,12 @@
 package Chapter_10_Object_Oriented_Thinking;
 
+import Utilities.MyStringBuilder1;
+
 /**
  * Implement the StringBuilder class
  * The StringBuilder class is provided in the Java library. 
- * Provide your own implementation for the following methods (name the new class MyStringBuilder1):
+ * Provide your own implementation for the following methods 
+ * (name the new class MyStringBuilder1):
  * public MyStringBuilder1(String s);
  * public MyStringBuilder1 append(MyStringBuilder1 s);
  * public MyStringBuilder1 append(int i);
@@ -22,6 +25,8 @@ public class Programming_Exercise_27
 {
 	public static void main(String[] args) 
 	{
-		
+		MyStringBuilder1 myString = new MyStringBuilder1("Hello World");
+		myString.append(100);
+		System.out.print(myString.toString());
 	}
 }
