e829905e8abbadab3aee4720811ea3695cf5c890
Kevin Guan <Kevin Guan>
2016-11-26 22:03:48 -0500

    Completed CHP10 PE28

--- src/Chapter_10_Object_Oriented_Thinking/Programming_Exercise_28.java
+++ src/Chapter_10_Object_Oriented_Thinking/Programming_Exercise_28.java
@@ -1,10 +1,12 @@
 package Chapter_10_Object_Oriented_Thinking;
 
+import Utilities.MyStringBuilder2;
 
 /**
  * Implement the StringBuilder class
  * The StringBuilder class is provided in the Java library. 
- * Provide your own implementation for the following methods (name the new class MyStringBuilder2):
+ * Provide your own implementation for the following methods 
+ * (name the new class MyStringBuilder2):
  * public MyStringBuilder2();
  * public MyStringBuilder2(char[] chars);
  * public MyStringBuilder2(String s);
@@ -13,7 +15,7 @@ package Chapter_10_Object_Oriented_Thinking;
  * public MyStringBuilder2 substring(int begin);
  * public MyStringBuilder2 toUpperCase();
  * 
- * 11/10/2016
+ * 11/27/2016
  * @author kevgu
  *
  */
@@ -22,6 +24,12 @@ public class Programming_Exercise_28
 {
 	public static void main(String[] args) 
 	{
-		
+		MyStringBuilder2 string1 = new MyStringBuilder2("Hello");
+		MyStringBuilder2 string2 = new MyStringBuilder2("World");
+		string1 = string1.insert(3, string2);
+		string1 = string1.reverse();
+		string1 = string1.substring(5);
+		string1 = string1.toUpperCase();
+		System.out.print(string1.toString());
 	}
 }
