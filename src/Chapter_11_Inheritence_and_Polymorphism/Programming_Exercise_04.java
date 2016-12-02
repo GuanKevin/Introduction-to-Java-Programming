aa5e71a85cca5b9ecd441fae4292b036e3ebbe1e
Kevin Guan <Kevin Guan>
2016-12-02 14:38:45 -0500

    Completed CHP11 PE04

--- src/Chapter_11_Inheritence_and_Polymorphism/Programming_Exercise_04.java
+++ src/Chapter_11_Inheritence_and_Polymorphism/Programming_Exercise_04.java
@@ -1,7 +1,8 @@
 package Chapter_11_Inheritence_and_Polymorphism;
-import java.util.Scanner;
-import java.util.Random;
+
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  * Maximum element in ArrayList
@@ -11,76 +12,52 @@ import java.util.ArrayList;
  * Write a test program that prompts the user to enter a sequence of numbers ending with 0, 
  * and invokes this method to return the largest number in the input.
  *
- * 03/20/2016
+ * 12/02/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_04 
 {
-	static Scanner input = new Scanner(System.in);
-	static Random rand = new Random();
 	public static void main(String[] args) 
 	{
-		ArrayList<Integer> list = new ArrayList<Integer>();
-		insertRandomNumbers(list);
-		System.out.println(max(list));
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter size ofd the list: ");
+		ArrayList<Integer> list = fillList(input.nextInt()); 
+		System.out.println("The largest number in the list is " + max(list) + ".");
+		
+		input.close();
 	}
 	
 	public static Integer max(ArrayList<Integer> list)
 	{
-		Integer maxEle = 0;
-		
-		for (int i = 0; i < list.size(); i++)
-		{
-			if (list.get(i) > maxEle)
-			{
-				maxEle = list.get(i);
-			}
-		}
-		
 		if (list.size() == 0)
-		{
 			return null;
-		}
 		
-		return maxEle;
-	}
-	
-	public static void insertRandomNumbers(ArrayList<Integer> list)
-	{
-		int number = rand.nextInt(100);
-		int counter = 0;
-		
-		while (number != 0)
-		{
-			counter++;
-			list.add(number);
-			
-			if (counter == 10)
-			{
-				System.out.println(number + " ");
-				counter = 1;
-			}
-			System.out.print(number + " ");
-			number = rand.nextInt(100);	
-		}
-		
-		System.out.println();
-	}
-	
-	public static void findMaxElement(ArrayList<Integer> list)
-	{
-		int maxEle = 0;
+		int max = Integer.MIN_VALUE;
 		
 		for (int i = 0; i < list.size(); i++)
-		{
-			if (list.get(i) > maxEle)
-			{
-				maxEle = list.get(i);
-			}
-		}
-		System.out.println("The max element is: " + maxEle);
+			if (max < list.get(i))
+				max = list.get(i);
+		
+		return max;
 	}
 	
+	public static ArrayList<Integer> fillList(int size)
+	{
+		Random randNum = new Random();
+		ArrayList<Integer> tempList = new ArrayList<Integer>();
+		
+		System.out.print("Adding numbers: ");
+		for (int i = 0; i <size; i++)
+		{
+			int number = randNum.nextInt(100) * 10;
+			System.out.print(number + " ");
+			tempList.add(number);
+		}
+		System.out.println();
+		
+		return tempList;
+	}
 }
