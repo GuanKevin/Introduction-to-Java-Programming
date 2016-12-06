a7513417cc78de7ff98c36cd8ee0cf36fb01b1c9
Kevin Guan <Kevin Guan>
2016-12-06 11:52:13 -0500

    Completed CHP11 PE12

--- src/Chapter_11_Inheritence_and_Polymorphism/Programming_Exercise_12.java
+++ src/Chapter_11_Inheritence_and_Polymorphism/Programming_Exercise_12.java
@@ -1,6 +1,7 @@
 package Chapter_11_Inheritence_and_Polymorphism;
-import java.util.Scanner;
+
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Sum ArrayList
@@ -8,46 +9,36 @@ import java.util.ArrayList;
  * public static double sum(ArrayList<Double> list) 
  * Write a test program that prompts the user to enter 5 numbers, stores them in an array list, and displays their sum.
  *
- * 03/14/2016
+ * 12/06/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_12 
 {
-	static Scanner input = new Scanner(System.in);
 	public static void main(String[] args) 
 	{
-		ArrayList<Double> list = new ArrayList<Double>();
-		inputToList(list);
-	}
-	
-	public static void inputToList(ArrayList<Double> list)
-	{
-		System.out.println("Enter size of list: ");
-		int size = input.nextInt();
-		System.out.println("Enter " + size + " numbers: ");
-		for (int i = 0; i < size; i++)
-		{
-			list.add(input.nextDouble());
-		}
+		Scanner input = new Scanner(System.in);
+		ArrayList<Double> list = new ArrayList<>();
 		
-		System.out.print("The sum of the list is " + sum(list));
+		System.out.print("Enter 5 numbers: ");
+		for (int i = 0; i < 5; i++)
+			list.add(input.nextDouble());
+		
+		System.out.print("The sum is: " + sum(list));
+		
+		input.close();
 	}
 	
 	public static double sum(ArrayList<Double> list) 
 	{
+		if (list.isEmpty())
+			return 0d;
+		
 		double sum = 0;
 		
 		for (int i = 0; i < list.size(); i++)
-		{
 			sum += list.get(i);
-		}
-		
-		/*for (Double i : list)
-		{
-			sum += i;
-		}*/
 		
 		return sum;
 	}
