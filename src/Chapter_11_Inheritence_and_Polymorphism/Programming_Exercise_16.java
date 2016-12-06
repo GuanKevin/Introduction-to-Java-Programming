49a71642bc07f40e379bc5afc979a6c53f0db06e
Kevin Guan <Kevin Guan>
2016-12-06 12:52:33 -0500

    Completed CHP11 PE16

--- src/Chapter_11_Inheritence_and_Polymorphism/Programming_Exercise_16.java
+++ src/Chapter_11_Inheritence_and_Polymorphism/Programming_Exercise_16.java
@@ -1,35 +1,44 @@
 package Chapter_11_Inheritence_and_Polymorphism;
-import java.util.Scanner;
+
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Addition quiz
  * Rewrite Listing 5.1 RepeatAdditionQuiz.java to alert the user if an answer is entered again. 
  * Hint: use an array list to store answers.
  *
- * 03/15/2016
+ * 12/06/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_16 
 {
-	static Scanner input = new Scanner(System.in);
 	public static void main(String[] args) 
 	{
-		ArrayList<Integer> additionList = new ArrayList<Integer>();
-		additionList.add(0, (int)(Math.random() * 10));
-		additionList.add(1, (int)(Math.random() * 10));
-		
-		System.out.print("What is " + additionList.get(0) + " + " + additionList.get(1) + "? ");
-		additionList.add(2, input.nextInt());
+		ArrayList<Integer> list = new ArrayList<Integer>();
+	    
+	    int number1 = (int) (Math.random() * 10);
+	    int number2 = (int) (Math.random() * 10);
 
-		while (additionList.get(0) + additionList.get(1) != additionList.get(2)) 
-		{
-			System.out.print("Wrong answer. Try again. What is " + additionList.get(0) + " + " + additionList.get(1) + "? ");
-			additionList.add(2, input.nextInt());;
-		}
-		
-		System.out.println("You got it!");
+	    Scanner input = new Scanner(System.in);
+	    System.out.print("What is " + number1 + " + " + number2 + "? ");
+	    int answer = input.nextInt();
+	    list.add(answer);
+	      
+	    while (number1 + number2 != answer) 
+	    {
+	    	System.out.print("Wrong. Try again. What is " + number1 + " + " + number2 + "? ");
+	    	answer = input.nextInt();
+	      
+	    	if (list.contains(answer)) 
+	    		System.out.println("You already entered " + answer);
+	    	else 
+	    	  list.add(answer);   
+	    }	    
+	    System.out.println("You got it!");
+	    
+	    input.close();
 	}
 }
