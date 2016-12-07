a82d42a2ef6e40dc48ddc1197681526d90fe74e2
Kevin Guan <Kevin Guan>
2016-12-07 11:55:44 -0500

    Completed CHP12 PE06

--- src/Chapter_12_Exception_Handling_and_Text_IO/Programming_Exercise_06.java
+++ src/Chapter_12_Exception_Handling_and_Text_IO/Programming_Exercise_06.java
@@ -1,4 +1,5 @@
 package Chapter_12_Exception_Handling_and_Text_IO;
+
 import java.util.Scanner;
 
 /**
@@ -6,51 +7,47 @@ import java.util.Scanner;
  * Listing 6.8 implements the hex2Dec(String hexString) method, which converts a hex string into a decimal number. 
  * Implement the hex2Dec method to throw a NumberFormatException if the string is not a hex string.
  * 
- * 04/
+ * 12/07/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_06
 {
-	static Scanner input = new Scanner(System.in);
 	public static void main(String[] args) 
 	{
 		try
 		{
-			System.out.print("Enter a hex number: ");
-			String hex = input.nextLine();
-			Integer.parseInt(hex, 16);
-			System.out.println("The decimal value for hex number " + hex + " is " + hexToDecimal(hex.toUpperCase()));
+			Scanner input = new Scanner(System.in);
+			System.out.print("Enter a hexadecimal number: ");
+			String hex = input.next().toUpperCase();
+			System.out.println("The decimal value of " + hex + " is " + hexToDec(hex));
+			
+			input.close();
 		}
-		catch (NumberFormatException ex)
+		catch (Exception ex)
 		{
-			System.out.println("Number Format Exception! Not a hexadecimal input!");
+			System.out.println(ex.getMessage());
 		}
-	}
-
-	public static int hexToDecimal(String hex) 
-	{		
-		int decimalValue = 0;
-		
-		for (int i = 0; i < hex.length(); i++) 
-		{
-			char hexChar = hex.charAt(i);
-			decimalValue = decimalValue * 16 + hexCharToDecimal(hexChar);
-		}
-		
-		return decimalValue;
 	}
 	
-	public static int hexCharToDecimal(char ch) 
+	public static int hexToDec(String hexString)
+	{
+		int value = convertHexToDec(hexString.charAt(0));
+		
+		for (int i = 1; i < hexString.length(); i++)
+			value = value * 16 + convertHexToDec(hexString.charAt(i));
+		
+		return value;
+	}
+	
+	public static int convertHexToDec(char ch)
 	{
 		if (ch >= 'A' && ch <= 'F')
-		{
 			return 10 + ch - 'A';
-		}
-		else // ch is '0', '1', ..., or '9'
-		{
+		else if (ch >= '0' && ch <= '9')
 			return ch - '0';
-		}
+		else
+			throw new NumberFormatException("Illegal character " + ch);
 	}
 }
