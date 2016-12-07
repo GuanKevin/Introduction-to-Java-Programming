393393cf230c7a314dfc24eb44098fab05704ed8
Kevin Guan <Kevin Guan>
2016-12-07 15:49:17 -0500

    Completed CHP12 PE13

--- src/Chapter_12_Exception_Handling_and_Text_IO/Programming_Exercise_13.java
+++ src/Chapter_12_Exception_Handling_and_Text_IO/Programming_Exercise_13.java
@@ -1,4 +1,5 @@
 package Chapter_12_Exception_Handling_and_Text_IO;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -9,50 +10,49 @@ import java.util.Scanner;
  * Words are separated by whitespace characters. 
  * The file name should be passed as a command-line argument, as shown in Figure 12.13.
  * 
- * 03/22/2016
+ * 12/07/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_13
 {
-	public static void main(String[] args) 
+	public static void main(String[] args) throws FileNotFoundException   
 	{
 		if (args.length != 1)
 		{
-			System.out.println("Not correct amount of arguments!" );
+			System.out.print("Missing arguments.");
 			System.exit(0);
 		}
 		
-		File testFile = new File(args[0]);
-		int charCounter = 0;
-		int wordCounter = 0;
-		int lineCounter = 0;
-		
-		try
+		File sourceFile = new File(args[0]);
+		if (!sourceFile.exists())
 		{
-			Scanner readFile = new Scanner(testFile);
-			
-			while (readFile.hasNext())
-			{
-				String holdFileLines = readFile.nextLine();
-				lineCounter++;
-				
-				charCounter += holdFileLines.length();
-				
-				String[] wordCheck = holdFileLines.split(" ");
-				wordCounter += wordCheck.length;
-			}
-			
-			readFile.close();
-		}
-		catch (FileNotFoundException ex)
-		{
-			System.out.println(ex.getStackTrace());
+			System.out.println("Source file does not exist.");
+			System.exit(1);
 		}
 		
-		System.out.print("Characters: " + charCounter + 
-				"\nWords: " + wordCounter +
-				"\nLines: " + lineCounter);
+		Scanner input = new Scanner(sourceFile);
+		int characters = 0, words = 0, lines = 0;
+		
+		while (input.hasNext())
+		{
+			String s = input.nextLine();
+			lines++;
+			
+			String[] split = s.split(" ");
+			
+			for (int i = 0; i < split.length; i++)
+				if (split[i].length() == 1)
+					characters++;
+				else
+					words++;
+		}
+		
+		System.out.print("Lines: " + lines +
+				"\nWords: " + words +
+				"\nCharacters: " + characters);
+		
+		input.close();
 	}
 }
