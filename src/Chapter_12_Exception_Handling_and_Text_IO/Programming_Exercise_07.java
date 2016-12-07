791f73e52bad612a2e50154c5d2891d931012fd2
Kevin Guan <Kevin Guan>
2016-12-07 12:06:52 -0500

    Completed CHP12 PE08

--- src/Chapter_12_Exception_Handling_and_Text_IO/Programming_Exercise_07.java
+++ src/Chapter_12_Exception_Handling_and_Text_IO/Programming_Exercise_07.java
@@ -16,6 +16,38 @@ public class Programming_Exercise_07
 {
 	public static void main(String[] args) 
 	{
+		try
+		{
+			Scanner input = new Scanner(System.in);
+			System.out.print("Enter a binary string: ");
+			String binaryStr = input.next();
+			System.out.print("The decimal of " + binaryStr + " is " + bin2Dec(binaryStr));
+			
+			input.close();
+		}
+		catch (Exception ex)
+		{
+			System.out.println(ex.getMessage());
+		}
+	}
+	
+	public static int bin2Dec(String binaryString)
+	{
+		int value = convertBin2Dec(Character.getNumericValue(binaryString.charAt(binaryString.length() - 1)), 0);
 		
+		for (int i = binaryString.length() - 2; i >= 0; i--)
+			value += convertBin2Dec(Character.getNumericValue(binaryString.charAt(i)), binaryString.length() - i - 1);
+		
+		return value;
+	}
+	
+	public static int convertBin2Dec(int binary, int pow)
+	{
+		if (binary == 0)
+			return 0;
+		else if (binary == 1)
+			return (int) Math.pow(2, pow);
+		else
+			throw new NumberFormatException(binary + " is not a binary.");
 	}
 }
