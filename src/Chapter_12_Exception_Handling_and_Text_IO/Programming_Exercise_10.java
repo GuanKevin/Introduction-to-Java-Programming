a81b29165406622eca3f08dba15cc2bb343cc28c
Kevin Guan <Kevin Guan>
2016-12-07 12:45:36 -0500

    Completed CHP12 PE10

--- src/Chapter_12_Exception_Handling_and_Text_IO/Programming_Exercise_10.java
+++ src/Chapter_12_Exception_Handling_and_Text_IO/Programming_Exercise_10.java
@@ -1,25 +1,22 @@
 package Chapter_12_Exception_Handling_and_Text_IO;
-import java.util.Scanner;
 
 /**
  * OutOfMemoryError
  * Write a program that causes the JVM to throw an OutOfMemoryError and catches and handles this error.
  * 
- * 03/22/2016
+ * 12/07/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_10
 {
-	Scanner input = new Scanner(System.in);
+	@SuppressWarnings("unused")
 	public static void main(String[] args) 
-	{
-		@SuppressWarnings("unused")
-		int[] largeArray;
-		
+	{	
 		try
 		{
+			int[] largeArray;
 			largeArray = new int[(int) Math.pow(Integer.MAX_VALUE, 2)];
 		}
 		catch (OutOfMemoryError ex)
