35be06b42f4a235f10b548451e597212ff3ef54d
Kevin Guan <Kevin Guan>
2016-12-07 12:40:41 -0500

    Completed CHP12 PE09

--- src/Chapter_12_Exception_Handling_and_Text_IO/Programming_Exercise_09.java
+++ src/Chapter_12_Exception_Handling_and_Text_IO/Programming_Exercise_09.java
@@ -7,38 +7,62 @@ import java.util.Scanner;
  * Define a custom exception called BinaryFormatException. 
  * Implement the bin2Dec method to throw a BinaryFormatException if the string is not a binary string.
  * 
- * 03/22/2016
+ * 12/07/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_09
 {
-	static Scanner input = new Scanner(System.in);
-	public static void main(String[] args) throws BinaryFormatException
+	public static void main(String[] args) throws BinaryFormatExeption
 	{
-		System.out.println("Enter a binary number: ");
-		String binaryNum;
-		
 		try
 		{
-			binaryNum = input.next();
-			testBinary(binaryNum);
+			Scanner input = new Scanner(System.in);
+			System.out.print("Enter a binary string: ");
+			String binaryStr = input.next();
+			System.out.print("The decimal of " + binaryStr + " is " + bin2Dec(binaryStr));
+			
+			input.close();
 		}
-		catch(NumberFormatException ex)
+		catch (Exception ex)
 		{
 			System.out.println(ex.getMessage());
 		}
 	}
 	
-	public static void testBinary(String binaryNum) throws BinaryFormatException
+	public static int bin2Dec(String binaryString) throws BinaryFormatExeption
 	{
-		for (int i = 0; i < binaryNum.length(); i++)
-		{
-			if (!(binaryNum.charAt(i) == '0' || binaryNum.charAt(i) == '1'))
-			{
-				throw new BinaryFormatException("This is not a binary string!");
-			}
-		}
+		int value = convertBin2Dec(Character.getNumericValue(binaryString.charAt(binaryString.length() - 1)), 0);
+		
+		for (int i = binaryString.length() - 2; i >= 0; i--)
+			value += convertBin2Dec(Character.getNumericValue(binaryString.charAt(i)), binaryString.length() - i - 1);
+		
+		return value;
+	}
+	
+	public static int convertBin2Dec(int binary, int pow) throws BinaryFormatExeption
+	{
+		if (binary == 0)
+			return 0;
+		else if (binary == 1)
+			return (int) Math.pow(2, pow);
+		else
+			throw new BinaryFormatExeption();
+	}
+}
+
+class BinaryFormatExeption extends Exception 
+{
+	private static final long serialVersionUID = 1L;
+
+	BinaryFormatExeption() 
+	{
+		super("Illegal binary character");
+	}
+
+	BinaryFormatExeption(String message) 
+	{
+	    super(message);
 	}
 }
