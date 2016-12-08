be0d6b05f030afb5a602b1bbbbc26eb79bd3f20d
Kevin Guan <Kevin Guan>
2016-12-08 15:07:46 -0500

    Completed CHP12 PE18

--- src/Chapter_12_Exception_Handling_and_Text_IO/Programming_Exercise_18.java
+++ src/Chapter_12_Exception_Handling_and_Text_IO/Programming_Exercise_18.java
@@ -1,4 +1,7 @@
 package Chapter_12_Exception_Handling_and_Text_IO;
+
+import java.io.File;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 /**
@@ -9,17 +12,70 @@ import java.util.Scanner;
  * The root directory and chapteri directory may contain other folders and files. Use the following command to run the program: 
  * java Exercise12_18 srcRootDirectory
  * 
- * 04/
+ * 12/08/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_18
 {
-	Scanner input = new Scanner(System.in);
-	public static void main(String[] args) 
+	public static void main(String[] args) throws Exception 
 	{
+		if (args.length != 1)
+		{
+			System.out.print("Enter source root directory.");
+			System.exit(0);
+		}
 		
+		File currentDir = new File(args[0]);
+		
+		if (!currentDir.exists())
+		{
+			System.out.println("Current directory " + args[0] + " does not exist.");
+			System.exit(1);
+		}
+		
+		if (!currentDir.isDirectory())
+		{
+			System.out.print(args[0] + " is not a directory.");
+			System.exit(2);
+		}
+		else 
+		{
+			File[] files = currentDir.listFiles();
+			
+			for (int i = 0; i < files.length; i++)
+				if (files[i].isDirectory() && files[i].getName().startsWith("chapter")) 
+				{
+					String packageStatment = "package " + files[i].getName() + ";\n";
+					insertIntoFilesInDirectory(files[i], packageStatment);
+		        }
+	    }
 	}
+	
+	private static void insertIntoFilesInDirectory(File directory, String line) throws Exception 
+	{
+		File[] files = directory.listFiles();
 
+		for (int i = 0; i < files.length; i++)
+			if (files[i].isFile() && files[i].getName().endsWith(".java")) 
+			{
+				insertPackageStatment(files[i], line);
+			}
+	}
+	
+	private static void insertPackageStatment(File file, String line) throws Exception 
+	{
+		StringBuilder sb = new StringBuilder(line);
+
+		try (Scanner input = new Scanner(file);) 
+		{
+			while (input.hasNext())  
+				sb.append("\n" + input.nextLine());
+	    }
+		try (PrintWriter output = new PrintWriter(file);) 
+		{
+			output.printf("%s\n", sb.toString());
+		}
+	}
 }
