0807d043436548fbbe397640fd4686d8115a96ea
Kevin Guan <Kevin Guan>
2016-12-09 15:12:26 -0500

    Completed CHP12 PE26

--- src/Chapter_12_Exception_Handling_and_Text_IO/Programming_Exercise_26.java
+++ src/Chapter_12_Exception_Handling_and_Text_IO/Programming_Exercise_26.java
@@ -1,4 +1,6 @@
 package Chapter_12_Exception_Handling_and_Text_IO;
+
+import java.io.File;
 import java.util.Scanner;
 
 /**
@@ -6,17 +8,25 @@ import java.util.Scanner;
  * Write a program that prompts the user to enter a directory name and creates a directory using the File’s mkdirs method. 
  * The program displays the message “Directory created successfully” if a directory is created or “Directory already exists” if the directory already exists.
  * 
- * 04/
+ * 12/09/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_26
 {
-	Scanner input = new Scanner(System.in);
 	public static void main(String[] args) 
 	{
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter name of directory: ");
+		String dirName = input.nextLine();
 		
+		if (new File(dirName).mkdir())
+			System.out.print("Directory " + dirName + " created.");
+		else
+			System.out.print("Directory " + dirName + " already exist.");
+		
+		
+		input.close();
 	}
-
 }
