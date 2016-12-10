8f08e071d5e6a51e62dfbd8c820eb7ae017fc2e8
Kevin Guan <Kevin Guan>
2016-12-10 18:50:57 -0500

    Completed CHP13 PE03

--- src/Chapter_13_Abstract_Classes_and_Interfaces/Programming_Exercise_03.java
+++ src/Chapter_13_Abstract_Classes_and_Interfaces/Programming_Exercise_03.java
@@ -1,4 +1,7 @@
 package Chapter_13_Abstract_Classes_and_Interfaces;
+
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -6,15 +9,58 @@ import java.util.Scanner;
  * Write the following method that sorts an ArrayList of numbers. 
  * public static void sort(ArrayList<Number> list)
  * 
+ * 12/10/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_03
 {
-	Scanner input = new Scanner(System.in);
 	public static void main(String[] args) 
 	{
+		ArrayList<Number> list = new ArrayList<>();
 		
+		System.out.print("Enter size of list: ");
+		Scanner input = new Scanner(System.in);
+		list = generateNumbers(input.nextInt());
+		
+		System.out.println(list.toString());
+		sort(list);
+		System.out.println(list.toString());
+		
+		input.close();
+	}
+	
+	public static void sort(ArrayList<Number> list)
+	{
+		int minNum;
+		
+		for (int i = 0; i < list.size() - 1; i++)
+		{
+			minNum = i;
+			
+			for (int j = i + 1; j < list.size(); j++)
+				if (list.get(minNum).intValue() > list.get(j).intValue())
+					minNum = j;
+			
+			if (minNum != i)
+			{
+				Number tempNum = list.get(minNum);
+				list.set(minNum, list.get(i));
+				list.set(i, tempNum);
+			}
+		}
+	}
+	
+	public static ArrayList<Number> generateNumbers(int size)
+	{
+		Random randNum = new Random();
+		
+		ArrayList<Number> tempList = new ArrayList<>();
+		
+		for (int i = 0; i < size; i++)
+			tempList.add(randNum.nextInt(size));
+		
+		return tempList;
 	}
 }
