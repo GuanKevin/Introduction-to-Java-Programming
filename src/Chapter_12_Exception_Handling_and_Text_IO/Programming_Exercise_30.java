c08c18790abc6724151b7c290e10b04bc0ad67b7
Kevin Guan <Kevin Guan>
2016-12-09 22:15:06 -0500

    Completed CHP12 PE30

--- src/Chapter_12_Exception_Handling_and_Text_IO/Programming_Exercise_30.java
+++ src/Chapter_12_Exception_Handling_and_Text_IO/Programming_Exercise_30.java
@@ -1,5 +1,9 @@
 package Chapter_12_Exception_Handling_and_Text_IO;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * Occurrences of each letter
  * Write a program that prompts the user to enter a file name and displays the occurrences of each letter in the file. 
@@ -12,8 +16,50 @@ package Chapter_12_Exception_Handling_and_Text_IO;
 
 public class Programming_Exercise_30
 {
-	public static void main(String[] args) 
+	public static void main(String[] args) throws FileNotFoundException   
 	{
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the name of the file: ");
+		File fileName = new File(input.nextLine());
 		
+		if (!fileName.exists())
+		{
+			System.out.print("File does not exist, terminating program.");
+			System.exit(0);
+		}
+		
+		int[] letterCount = new int[52];
+		
+		countLetter(fileName, letterCount);
+		displayCount(letterCount);
+		
+		input.close();
+	}
+	
+	public static void countLetter(File fileName, int[] letterCount) throws FileNotFoundException
+	{
+		try (Scanner reader = new Scanner(fileName);)
+		{
+			while (reader.hasNext())
+			{
+				String fileLine = reader.nextLine();
+				
+				for (int i = 0; i < fileLine.length(); i++)
+					if (Character.isAlphabetic(fileLine.charAt(i)))
+						if (fileLine.charAt(i) <= 90)
+							letterCount[fileLine.charAt(i) - 65]++;
+						else if (fileLine.charAt(i) <= 122)
+							letterCount[fileLine.charAt(i) - 71]++;
+			}
+		}
+	}
+	
+	public static void displayCount(int [] letterCount)
+	{
+		for (int i = 0; i < 26; i++)
+			System.out.println((char)(i + 'A') + "'s is " + letterCount[i]);
+		
+		for (int i = 26; i < 52; i++)
+			System.out.println((char)(i + 71) + "'s is " + letterCount[i]);
 	}
 }
--- src/Chapter_12_Exception_Handling_and_Text_IO/Programming_Exercise_31.java
+++ src/Chapter_12_Exception_Handling_and_Text_IO/Programming_Exercise_31.java
@@ -1,65 +1,28 @@
 package Chapter_12_Exception_Handling_and_Text_IO;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
 
 /**
  * Baby name popularity ranking
- * The popularity ranking of baby names from years 2001 to 2010 is downloaded from www.ssa.gov/oact/babynames and stored in files named babynameranking2001.txt, babynameranking2002.txt, . . . , babynameranking2010.txt. 
+ * The popularity ranking of baby names from years 2001 to 2010 is downloaded from www.ssa.gov/oact/babynames and stored in files named babynameranking2001.txt, 
+ * babynameranking2002.txt, . . . , babynameranking2010.txt. 
  * Each file contains one thousand lines. 
  * Each line contains a ranking, a boy’s name, number for the boy’s name, a girl’s name, and number for the girl’s name.
  * So, the boy’s name Jacob and girl’s name Isabella are ranked #1 and the boy’s name Ethan and girl’s name Sophia are ranked #2. 
  * 21,875 boys are named Jacob and 22,731 girls are named Isabella. 
- * Write a program that prompts the user to enter the year, gender, and followed by a name, and displays the ranking of the name for the year.
+ * Write a program that prompts the user to enter the year, gender, and followed by a name, 
+ * and displays the ranking of the name for the year.
  * 
- * 04/
+ * 12/09/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_31
 {
-	static Scanner input = new Scanner(System.in);
-	public static void main(String[] args) 
+	public static void main(String[] args)   
 	{
-		File[] file = new File[10];
-		for(int i = 1; i <= 10; i++){
-			if(i == 10)
-				file[i -1] = new File("babynamesranking20" + i + ".txt");
-			else
-				file[i - 1] = new File("babynamesranking200" + i + ".txt");
-		}
-		if(file[1].exists()){
-			System.out.println("Exists");
-		}
-		System.out.print("Enter the year: ");
-		int year = Integer.parseInt(input.nextLine());
-		System.out.print("Enter the gender: ");
-		char gender = input.nextLine().charAt(0);
-		System.out.print("Enter the name: ");
-		String name = input.nextLine();
-		System.out.println(name + " is ranked " + getRank(file, year, gender, name) + " in year " + year);
-	}
-	public static int getRank(File[] file, int year, char gender, String name)
-	{
-		Scanner inputf = null;
-		String line = null;
-		String temp;
-		temp = name.toLowerCase();
-		temp = name.substring(0, 1).toUpperCase() + name.substring(1, name.length());
-		try {
-			inputf = new Scanner(file[year - 2001]);
-		} catch (FileNotFoundException e) {
-			System.out.println(e.getMessage());
-		}
-		while(inputf.hasNext())
+		if (args.length == 0)
 		{
-			line = inputf.nextLine();
-			if(line.contains(temp))
-				return Integer.parseInt(line.substring(0, line.indexOf(" ")));
+			System.out.print("Current directory is empty, terminating program.");
 		}
-		System.out.println(name + " is not ranked in year " + year);
-		System.exit(0);
-		return 0;
 	}
 }
