71aeff346aa4905a7935ad3c633c0f10494bd8bc
Kevin Guan <Kevin Guan>
2016-12-09 20:06:38 -0500

    Completed CHP12 PE27

--- src/Chapter_12_Exception_Handling_and_Text_IO/Programming_Exercise_27.java
+++ src/Chapter_12_Exception_Handling_and_Text_IO/Programming_Exercise_27.java
@@ -1,5 +1,6 @@
 package Chapter_12_Exception_Handling_and_Text_IO;
-import java.util.Scanner;
+
+import java.io.File;
 
 /**
  * Replace words
@@ -10,17 +11,30 @@ import java.util.Scanner;
  * Use the following command to run your program. 
  * java Exercise12_27 *
  * 
- * 04/
+ * 12/09/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_27
 {
-	Scanner input = new Scanner(System.in);
 	public static void main(String[] args) 
-	{
+	{		
+		if (args.length == 0)
+		{
+			System.out.print("Missing files.");
+			System.exit(0);
+		}
 		
+		for (String str : args)
+			if (str.matches("Exercise[\\d]_[\\d].txt"))
+			{
+				StringBuilder strBld = new StringBuilder(str);
+				strBld.insert(str.indexOf('_') - 1, '0');
+				strBld.insert(11, '0');
+				
+				new File(str).renameTo(new File(strBld.toString()));
+				System.out.println(str + " renamed to " + strBld.toString());
+			}
 	}
-
 }
