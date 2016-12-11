a89f2309e130c742c02f5133e6fd216e199b9eae
Kevin Guan <Kevin Guan>
2016-12-10 23:46:42 -0500

    Completed CHP13 PE10

--- src/Chapter_13_Abstract_Classes_and_Interfaces/Programming_Exercise_10.java
+++ src/Chapter_13_Abstract_Classes_and_Interfaces/Programming_Exercise_10.java
@@ -1,22 +1,25 @@
 package Chapter_13_Abstract_Classes_and_Interfaces;
-import java.util.Scanner;
+
+import Utilities.Rectangle;
 
 /**
  * Enable Rectangle comparable
  * Rewrite the Rectangle class in Listing 13.3 to extend GeometricObject and implement the Comparable interface. 
  * Override the equals method in the Object class. 
  * Two Rectangle objects are equal if their areas are the same. 
- * Draw the UML diagram that involves Rectangle, GeometricObject, and Comparable.
  * 
+ * 12/10/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_10
 {
-	Scanner input = new Scanner(System.in);
 	public static void main(String[] args) 
 	{
+		Rectangle r1 = new Rectangle(4, 4);
+		Rectangle r2 = new Rectangle(4, 3);
 		
+		System.out.print(r1.equals(r2));
 	}
 }
---
+++ src/Utilities/Rectangle.java
@@ -0,0 +1,75 @@
+package Utilities;
+
+public class Rectangle extends GeometricObject 
+{
+	private double width;
+	private double height;
+
+	public Rectangle() 
+	{
+		
+	}
+
+	public Rectangle(double width, double height) 
+	{
+		this.width = width;
+	    this.height = height;
+	}
+
+	/** Return width */
+	public double getWidth() 
+	{
+		return width;
+	}
+
+	/** Set a new width */
+	public void setWidth(double width) 
+	{
+		this.width = width;
+	}
+
+	/** Return height */
+	public double getHeight() 
+	{
+		return height;
+	}
+
+	/** Set a new height */
+	public void setHeight(double height) 
+	{
+		this.height = height;
+	}
+
+	/** Return area */
+	public double getArea() 
+	{
+		return width * height;
+	}
+
+	/** Return perimeter */
+	public double getPerimeter() 
+	{
+		return 2 * (width + height);
+	}
+
+	@Override
+	public int compareTo(GeometricObject o) 
+	{
+		if (this.getArea() > o.getArea())
+			return 1;
+		else if (this.getArea() < o.getArea())
+			return -1;
+		return 0;
+	}
+	
+	public boolean equals (GeometricObject o)
+	{
+		return this.getArea() == ((Rectangle) o).getArea();
+	}
+
+	@Override
+	public void howToColor() 
+	{
+		System.out.println("Rectangle: Draw around it!");
+	}
+}
