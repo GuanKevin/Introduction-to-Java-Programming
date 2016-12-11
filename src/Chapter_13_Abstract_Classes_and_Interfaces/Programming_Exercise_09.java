8f8cb45164e8b4412cb9787414de9cb81bddf048
Kevin Guan <Kevin Guan>
2016-12-10 23:36:14 -0500

    Completed CHP13 PE09

--- src/Chapter_13_Abstract_Classes_and_Interfaces/Programming_Exercise_09.java
+++ src/Chapter_13_Abstract_Classes_and_Interfaces/Programming_Exercise_09.java
@@ -1,21 +1,24 @@
 package Chapter_13_Abstract_Classes_and_Interfaces;
-import java.util.Scanner;
+
+import Utilities.Circle;
 
 /**
  * Enable Circle comparable
  * Rewrite the Circle class in Listing 13.2 to extend GeometricObject and implement the Comparable interface. 
  * Override the equals method in the Object class. Two Circle objects are equal if their radii are the same. 
- * Draw the UML diagram that involves Circle, GeometricObject, and Comparable.
  * 
+ * 12/10/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_09
 {
-	Scanner input = new Scanner(System.in);
 	public static void main(String[] args) 
 	{
+		Circle c1 = new Circle(5);
+		Circle c2 = new Circle(15);
 		
+		System.out.print(c1.equals(c2));
 	}
 }
--- src/Utilities/Circle.java
+++ src/Utilities/Circle.java
@@ -2,64 +2,74 @@ package Utilities;
 
 public class Circle extends GeometricObject 
 {
-	  private double radius;
+	private double radius;
 
-	  /**Default constructor*/
-	  public Circle() {
-	    this(1.0);
-	  }
+	/**Default constructor*/
+	public Circle() 
+	{
+		this(1.0);
+	}
 
-	  /**Construct circle with a specified radius*/
-	  public Circle(double radius) {
-	    this(radius, "white", false);
-	  }
+	/**Construct circle with a specified radius*/
+	public Circle(double radius) 
+	{
+		this(radius, "white", false);
+	}
 
-	  /**Construct a circle with specified radius, filled, and color*/
-	  public Circle(double radius, String color, boolean filled) {
-	    super(color, filled);
-	    this.radius = radius;
-	  }
+	/**Construct a circle with specified radius, filled, and color*/
+	public Circle(double radius, String color, boolean filled) 
+	{
+		super(color, filled);
+		this.radius = radius;
+	}
 
-	  /**Return radius*/
-	  public double getRadius() {
-	    return radius;
-	  }
+	/**Return radius*/
+	public double getRadius() 
+	{
+		return radius;
+	}
 
-	  /**Set a new radius*/
-	  public void setRadius(double radius) {
-	    this.radius = radius;
-	  }
+	/**Set a new radius*/
+	public void setRadius(double radius) 
+	{
+		this.radius = radius;
+	}
 
-	  @Override
-	  /**Implement the getArea method defined in GeometricObject*/
-	  public double getArea() {
-	    return radius*radius*Math.PI;
-	  }
+	@Override
+	/**Implement the getArea method defined in GeometricObject*/
+	public double getArea() 
+	{
+		return radius*radius*Math.PI;
+	}
 
-	  /**Implement the getPerimeter method defined in GeometricObject*/
-	  public double getPerimeter() {
-	    return 2*radius*Math.PI;
-	  }
+	/**Implement the getPerimeter method defined in GeometricObject*/
+	public double getPerimeter() 
+	{
+		return 2*radius*Math.PI;
+	}
 
-	  /**Override the equals() method defined in the Object class*/
-	  public boolean equals(Circle circle) {
-	    return this.radius == circle.getRadius();
-	  }
+	/**Override the equals() method defined in the Object class*/
+	public boolean equals(Circle circle) 
+	{
+		if (compareTo(circle) == 0)
+			return false;
+		else
+			return true;
+	}
 
-	  @Override 
-	  public String toString() {
-	    return "[Circle] radius = " + radius;
-	  }
+	@Override 
+	public String toString() 
+	{
+		return "[Circle] radius = " + radius;
+	}  
 
 	@Override
 	public int compareTo(GeometricObject o) 
 	{
-		if (getArea() > o.getArea())
+		if (o instanceof Circle && ((Circle) o).getRadius() == this.radius)
 			return 1;
-		else if (getArea() == o.getArea())
-			return 0;
 		else
-			return -1;
+			return 0;
 	}
 
 	@Override
