4ebe4ca7702447e05f5cc4123e26305c6cfba585
Kevin Guan <Kevin Guan>
2016-12-11 00:06:51 -0500

    Completed CHP13 PE11

--- src/Chapter_13_Abstract_Classes_and_Interfaces/Programming_Exercise_11.java
+++ src/Chapter_13_Abstract_Classes_and_Interfaces/Programming_Exercise_11.java
@@ -1,24 +1,28 @@
 package Chapter_13_Abstract_Classes_and_Interfaces;
-import java.util.Scanner;
+
+import Utilities.Octagon;
 
 /**
  * The Octagon class
  * Write a class named Octagon that extends GeometricObject and implements the Comparable and Cloneable interfaces. 
  * Assume that all eight sides of the octagon are of equal length. 
- * The area can be computed using the following formula:
- * Draw the UML diagram that involves Octagon, GeometricObject, Comparable, and Cloneable. 
+ * The area can be computed using the following formula: 
+ * area = (2 + 4 / root(2))* side * side
  * Write a test program that creates an Octagon object with side value 5 and displays its area and perimeter. 
  * Create a new object using the clone method and compare the two objects using the compareTo method.
  * 
+ * 12/10/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_11
 {
-	Scanner input = new Scanner(System.in);
-	public static void main(String[] args) 
+	public static void main(String[] args) throws CloneNotSupportedException 
 	{
+		Octagon o1 = new Octagon(8, "Blue");
+		Octagon o2 = (Octagon) o1.clone();
 		
+		System.out.print(o1.compareTo(o2) + " " + o1.getArea());
 	}
 }
