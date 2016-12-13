6625dbac530a72cb40144cfab10b19959e634088
Kevin Guan <Kevin Guan>
2016-12-13 15:26:50 -0500

    Completed CHP13 PE14-PE15

--- src/Chapter_13_Abstract_Classes_and_Interfaces/Programming_Exercise_14.java
+++ src/Chapter_13_Abstract_Classes_and_Interfaces/Programming_Exercise_14.java
@@ -1,5 +1,8 @@
 package Chapter_13_Abstract_Classes_and_Interfaces;
-import java.util.Scanner;
+
+import java.math.BigInteger;
+
+import Utilities.Rational;
 
 /**
  * Demonstrate the benefits of encapsulation
@@ -7,17 +10,25 @@ import java.util.Scanner;
  * Create an array of two integers as follows: 
  * private long[] r = new long[2];
  * Use r[0] to represent the numerator and r[1] to represent the denominator.
- * The signatures of the methods in the Rational class are not changed, so a client application that uses the previous Rational class can continue to use this new Rational class without being recompiled.
+ * The signatures of the methods in the Rational class are not changed, 
+ * so a client application that uses the previous Rational class can continue to use this new Rational class without being recompiled.
  * 
+ * 12/11/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_14
 {
-	Scanner input = new Scanner(System.in);
 	public static void main(String[] args) 
 	{
-		
+		Rational r1 = new Rational(new BigInteger("14"), new BigInteger("12"));
+		Rational r2 = new Rational(new BigInteger("21"), new BigInteger("7"));
+
+		System.out.println(r1 + " + " + r2 + " = " + r1.add(r2));
+		System.out.println(r1 + " - " + r2 + " = " + r1.subtract(r2));
+		System.out.println(r1 + " * " + r2 + " = " + r1.multiply(r2));
+		System.out.println(r1 + " / " + r2 + " = " + r1.divide(r2));
+		System.out.println(r2 + " is " + r2.doubleValue());		
 	}
 }
--- src/Chapter_13_Abstract_Classes_and_Interfaces/Programming_Exercise_15.java
+++ src/Chapter_13_Abstract_Classes_and_Interfaces/Programming_Exercise_15.java
@@ -1,19 +1,29 @@
 package Chapter_13_Abstract_Classes_and_Interfaces;
-import java.util.Scanner;
+
+import java.math.BigInteger;
+
+import Utilities.Rational;
 
 /**
  * Use BigInteger for the Rational class
  * Redesign and implement the Rational class in Listing 13.13 using BigInteger for the numerator and denominator.
  * 
+ * 12/14/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_15
 {
-	Scanner input = new Scanner(System.in);
 	public static void main(String[] args) 
 	{
+		Rational r1 = new Rational(new BigInteger("9457734514"), new BigInteger("131340"));
+		Rational r2 = new Rational(new BigInteger("2456867891"), new BigInteger("4234257"));
 		
+		System.out.println(r1 + " + " + r2 + " = " + r1.add(r2));
+		System.out.println(r1 + " - " + r2 + " = " + r1.subtract(r2));
+		System.out.println(r1 + " * " + r2 + " = " + r1.multiply(r2));
+		System.out.println(r1 + " / " + r2 + " = " + r1.divide(r2));
+		System.out.println(r2 + " is " + r2.doubleValue());
 	}
 }
