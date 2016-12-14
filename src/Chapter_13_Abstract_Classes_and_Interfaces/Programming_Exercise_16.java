1f7c369a016008b907c51f9e18f9e2e2b3612970
Kevin Guan <Kevin Guan>
2016-12-14 15:00:59 -0500

    Completed CHP13 PE16

--- src/Chapter_13_Abstract_Classes_and_Interfaces/Programming_Exercise_16.java
+++ src/Chapter_13_Abstract_Classes_and_Interfaces/Programming_Exercise_16.java
@@ -1,20 +1,56 @@
 package Chapter_13_Abstract_Classes_and_Interfaces;
-import java.util.Scanner;
+
+import java.math.BigInteger;
+
+import Utilities.Rational;
 
 /**
  * Create a rational-number calculator
  * Write a program similar to Listing 7.9, Calculator.java. Instead of using integers, use rationals, as shown in Figure 13.10a. 
- * You will need to use the split method in the String class, introduced in Section 10.10.3, Replacing and Splitting Strings, to retrieve the numerator string and denominator string, and convert strings into integers using the Integer.parseInt method.
+ * You will need to use the split method in the String class, introduced in Section 10.10.3, Replacing and Splitting Strings, 
+ * to retrieve the numerator string and denominator string, and convert strings into integers using the Integer.parseInt method.
  * 
+ * 12/14/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_16
 {
-	Scanner input = new Scanner(System.in);
 	public static void main(String[] args) 
 	{
+		if (args.length != 1)
+		{
+			System.out.print("\"Operand1 Operator Operand2\"");
+			System.exit(0);
+		}
 		
+		String[] tokens = args[0].split(" ");
+		
+		switch (tokens[1].charAt(0))
+		{
+			case '+': System.out.println(tokens[0] + " " + tokens[1] + " " + tokens[2] + " = " + 
+					getRational(tokens[0]).add(getRational(tokens[2])));
+					break;
+			case '-': System.out.println(tokens[0] + " " + tokens[1] + " " + tokens[2] + " = " + 
+					getRational(tokens[0]).subtract(getRational(tokens[2])));
+					break;
+			case '*': System.out.println(tokens[0] + " " + tokens[1] + " " + tokens[2] + " = " + 
+					getRational(tokens[0]).multiply(getRational(tokens[2])));
+					break;
+			case '/': System.out.println(tokens[0] + " " + tokens[1] + " " + tokens[2] + " = " + 
+					getRational(tokens[0]).divide(getRational(tokens[2])));
+					break;
+			default: System.out.print("Invalid operator!");
+					break;
+		}
+	}
+	
+	public static Rational getRational(String s)
+	{
+		String[] str = s.split("/");
+		BigInteger numerator = new BigInteger(str[0]);
+		BigInteger demoninator = new BigInteger(str[1]);
+		return new Rational(numerator, demoninator);
 	}
 }
