1c1b0997fcd7a0397130eebe46605f30d6dc2fd7
Kevin Guan <Kevin Guan>
2016-12-14 21:07:56 -0500

    Completed CHP13 PE21

--- src/Chapter_13_Abstract_Classes_and_Interfaces/Programming_Exercise_21.java
+++ src/Chapter_13_Abstract_Classes_and_Interfaces/Programming_Exercise_21.java
@@ -1,5 +1,9 @@
 package Chapter_13_Abstract_Classes_and_Interfaces;
 
+import java.math.BigInteger;
+
+import Utilities.Rational;
+
 /**
  * Algebra: vertex form equations
  * The equation of a parabola can be expressed in either standard form (y = ax2 + bx + c) or vertex form (y = a(x - h)2 + k). 
@@ -14,6 +18,13 @@ public class Programming_Exercise_21
 {
 	public static void main(String[] args) 
 	{
+		Rational a = new Rational(BigInteger.ONE, BigInteger.ONE);
+		Rational b = new Rational(new BigInteger("3"), BigInteger.ONE);
+		Rational c = new Rational(BigInteger.ONE, BigInteger.ONE);
 		
+		Rational h = new Rational(BigInteger.ZERO.subtract(b.getNumerator()), a.getNumerator().multiply(new BigInteger("2")));	// h = -b / 2a
+		Rational k = (a.multiply(h.multiply(h)).add(b.multiply(h))).add(c);														// k = f(h) = a * h^2 + b * h + c
+		
+		System.out.println("h is " + h + " k is " + k);
 	}
 }
