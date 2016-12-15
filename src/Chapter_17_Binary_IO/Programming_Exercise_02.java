6bafd2c877b66f96d02703c61a33b4e05603c250
Kevin Guan <Kevin Guan>
2016-12-15 17:38:18 -0500

    CHP17 PE02 Initial Commit

--- src/Chapter_17_Binary_IO/Programming_Exercise_02.java
+++ src/Chapter_17_Binary_IO/Programming_Exercise_02.java
@@ -1,9 +1,10 @@
 package Chapter_17_Binary_IO;
+
+import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Random;
-import java.util.Scanner;
 
 /**
  * Create a binary data file
@@ -11,26 +12,34 @@ import java.util.Scanner;
  * Append new data to it if it already exists. 
  * Write 100 integers created randomly into the file using binary I/O.
  * 
- * 
+ * 12/15/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_02
 {
-	static Scanner input = new Scanner(System.in);
-	public static void main(String[] args) 
+	public static void main(String[] args) throws IOException  
 	{
-		File file = new File("Excercise17_02.dat");
-		try{
-			PrintWriter input = new PrintWriter(file);
-			Random rand = new Random();
-			for(int i = 0; i < 100; i++){
-				input.print(rand.nextInt(2));
-			}
-			input.close();
-		} catch (FileNotFoundException e) {
-			System.out.println(e.getMessage());
+		File binDataFile = new File("src/Text_Files/Chapter_17/CHP17_PE02.dat");
+		if (!binDataFile.exists())
+		{
+			System.out.println("File doesn't exist, generating binary data file...");
+			binDataFile.createNewFile();
 		}
+		
+		try (DataOutputStream output = new DataOutputStream(new FileOutputStream(binDataFile, true));) 
+		{
+			Random rand = new Random();
+			System.out.println("Generating numbers to file...");
+			for (int i = 1; i <= 100; i++)
+			{
+				if (i % 10 == 0)
+					output.write((String.valueOf(rand.nextInt(100)) + " ").getBytes());
+				else
+					output.write((String.valueOf(rand.nextInt(100)) + "\n").getBytes());
+			}
+		}
+		System.out.println("Done");
 	}
 }
