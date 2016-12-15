34bac8c13ea22758f00a19cb27e6814f1db612d6
Kevin Guan <Kevin Guan>
2016-12-15 18:42:29 -0500

    CHP17 PE04 Initial Commit

--- src/Chapter_17_Binary_IO/Programming_Exercise_04.java
+++ src/Chapter_17_Binary_IO/Programming_Exercise_04.java
@@ -1,5 +1,13 @@
 package Chapter_17_Binary_IO;
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Convert a text file into UTF
@@ -8,16 +16,33 @@ import java.util.Scanner;
  * Use the following command to run the program:
  * java Exercise17_04 Welcome.java Welcome.utf
  * 
- * 
+ * 12/15/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_04
 {
-	Scanner input = new Scanner(System.in);
-	public static void main(String[] args) 
+	public static void main(String[] args) throws FileNotFoundException, IOException  
 	{
+		try (BufferedReader input = new BufferedReader(new FileReader(args[0])); DataOutputStream output = new DataOutputStream(new FileOutputStream(args[1]));)
+		{
+			String line;
+			
+			while ((line = input.readLine()) != null)
+			{
+				output.writeUTF(line);
+			}
+		}
 		
+		try (InputStream input = new FileInputStream(args[0]);)
+		{
+			System.out.println(args[0] + "'s size is " + input.available() + " bytes");
+			
+			try (InputStream input2 = new FileInputStream(args[1]);)
+			{
+				System.out.println(args[1] + "'s size is " + input2.available() + " bytes");
+			}
+		}
 	}
 }
