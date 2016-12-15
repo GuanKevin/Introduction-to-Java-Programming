4a6df6e0c4bb0933197467cdc6893403b09c924a
Kevin Guan <Kevin Guan>
2016-12-14 21:03:21 -0500

    Completed CHP13 PE20

--- src/Chapter_13_Abstract_Classes_and_Interfaces/Programming_Exercise_20.java
+++ src/Chapter_13_Abstract_Classes_and_Interfaces/Programming_Exercise_20.java
@@ -1,19 +1,39 @@
 package Chapter_13_Abstract_Classes_and_Interfaces;
-import java.util.Scanner;
+
+import Utilities.Complex;
 
 /**
  * Algebra: solve quadratic equations
  * Rewrite Programming Exercise 3.1 to obtain imaginary roots if the determinant is less than 0 using the Complex class in Programming Exercise 13.17.
  * 
+ * 12/14/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_20
 {
-	Scanner input = new Scanner(System.in);
 	public static void main(String[] args) 
 	{
+		double a = 1, b = 2, c = 3;
+		double discriminant = Math.pow(b, 2) - 4 * a * c;
 		
+		if (discriminant < 0)
+		{
+			Complex r1 = new Complex(-b / (2 * a), Math.sqrt(-discriminant) / (2 * a));
+			Complex r2 = new Complex(-b / (2 * a), -Math.sqrt(-discriminant) / (2 * a));
+			System.out.println("The root is " + r1 + " and " + r2);
+		}
+		else if (discriminant == 0)
+		{
+			double r1 = -b / (2 * a);
+			System.out.println("The root is " + r1);
+		}
+		else
+		{
+			double r1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+			double r2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+			System.out.println("The root is " + r1 + " and " + r2);
+		}
 	}
 }
--- src/Chapter_13_Abstract_Classes_and_Interfaces/Programming_Exercise_21.java
+++ src/Chapter_13_Abstract_Classes_and_Interfaces/Programming_Exercise_21.java
@@ -1,18 +1,17 @@
 package Chapter_13_Abstract_Classes_and_Interfaces;
-import java.util.Scanner;
 
 /**
  * Algebra: vertex form equations
  * The equation of a parabola can be expressed in either standard form (y = ax2 + bx + c) or vertex form (y = a(x - h)2 + k). 
  * Write a program that prompts the user to enter a, b, and c as integers in standard form and displays h and k in the vertex form.
  * 
+ * 12/14/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_21
 {
-	Scanner input = new Scanner(System.in);
 	public static void main(String[] args) 
 	{
 		
