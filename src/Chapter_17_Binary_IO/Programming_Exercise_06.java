2a8733e08e38575d73bf00a8f7a11d748da473f6
Kevin Guan <Kevin Guan>
2016-12-15 19:59:50 -0500

    CHP17 PE06 Initial Commit

--- src/Chapter_17_Binary_IO/Programming_Exercise_06.java
+++ src/Chapter_17_Binary_IO/Programming_Exercise_06.java
@@ -1,5 +1,11 @@
 package Chapter_17_Binary_IO;
-import java.util.Scanner;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+import Utilities.Loan;
 
 /**
  * Store Loan objects
@@ -7,16 +13,22 @@ import java.util.Scanner;
  * Rewrite the Loan class to implement Serializable. 
  * Write a program that creates five Loan objects and stores them in a file named Exercise17_06.dat.
  * 
- * 
+ * 12/15/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_06
 {
-	Scanner input = new Scanner(System.in);
-	public static void main(String[] args) 
+	public static void main(String[] args) throws FileNotFoundException, IOException 
 	{
-		
+		try (ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream("src/Text_Files/Chapter_17/CHP17_PE06.dat"));)
+		{
+			output.writeObject(new Loan());
+			output.writeObject(new Loan(4.5, 3, 2000));
+			output.writeObject(new Loan(5.5, 4, 1000));
+			output.writeObject(new Loan(.5, 1, 10000));
+			output.writeObject(new Loan(.1, 10, 990000));
+		}
 	}
 }
