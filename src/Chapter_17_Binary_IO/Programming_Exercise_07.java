5269801f727b417b80691b46420fe88d1ea3c006
Kevin Guan <Kevin Guan>
2016-12-15 20:17:37 -0500

    ChP17 PE07 Initial Commit

--- src/Chapter_17_Binary_IO/Programming_Exercise_07.java
+++ src/Chapter_17_Binary_IO/Programming_Exercise_07.java
@@ -1,5 +1,12 @@
 package Chapter_17_Binary_IO;
-import java.util.Scanner;
+
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+import Utilities.Loan;
 
 /**
  * Restore objects from a file
@@ -10,16 +17,35 @@ import java.util.Scanner;
  * Write a program that reads the Loan objects from the file and displays the total loan amount. 
  * Suppose you don’t know how many Loan objects are there in the file, use EOFException to end the loop.
  * 
- * 
+ * 12/15/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_07
 {
-	Scanner input = new Scanner(System.in);
-	public static void main(String[] args) 
+	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException 
 	{
-		
+		try (ObjectInputStream input = new ObjectInputStream(new FileInputStream("src/Text_Files/Chapter_17/CHP17_PE07.dat"));)
+		{
+			double totalLoan = 0;
+			boolean eof = true;
+			
+			try 
+			{
+				Loan loan;
+				while (eof)
+				{
+					loan = (Loan) input.readObject();
+					totalLoan += loan.getLoanAmount();
+				}
+			}
+			catch (EOFException ex)
+			{
+				eof = false;
+			}
+			
+			System.out.print("The total loan is " + totalLoan);
+		}
 	}
 }
