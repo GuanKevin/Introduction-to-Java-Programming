97cdeab84e306e8db60a01c2d823044f291cc4d4
Kevin Guan <Kevin Guan>
2016-12-16 14:30:34 -0500

    CHP17 PE01 update: remove static Scanner

--- src/Chapter_17_Binary_IO/Programming_Exercise_10.java
+++ src/Chapter_17_Binary_IO/Programming_Exercise_10.java
@@ -1,5 +1,4 @@
 package Chapter_17_Binary_IO;
-import java.util.Scanner;
 
 /**
  * Split files
@@ -9,14 +8,13 @@ import java.util.Scanner;
  * java Exercise17_10 SourceFile numberOfPieces
  * The command creates the files SourceFile.1, SourceFile.2, . . . , SourceFile.n, where n is numberOfPieces and the output files are about the same size.
  * 
- * 
+ * 12/15/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_10
 {
-	Scanner input = new Scanner(System.in);
 	public static void main(String[] args) 
 	{
 		
--- src/Chapter_18_Recursion/Programming_Exercise_01.java
+++ src/Chapter_18_Recursion/Programming_Exercise_01.java
@@ -1,39 +1,36 @@
 package Chapter_18_Recursion;
+
 import java.util.Scanner;
 
 /**
  * Factorial
  * Implement the factorial method using recursion. 
  * Write a program that prompts the user to enter an integer and displays its factorial.
+ * http://www.programmerinterview.com/index.php/recursion/explanation-of-recursion/
+ * https://www.mathsisfun.com/numbers/factorial.html
  * 
- * 06/25/2016
+ * 12/16/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_01
 {
-	static Scanner input = new Scanner(System.in);
 	public static void main(String[] args) 
 	{
+		Scanner input = new Scanner(System.in);
 		System.out.print("Enter a number: ");
 		int number = input.nextInt();
+		System.out.print("The factorial of " + number + "! is " + factorial(number));
 		
-		System.out.print("The factorial of " + number + " is " + factorial(number));
+		input.close();
 	}
 	
-	/*
-	 * Finds the factorial of the given number 
-	 * 4! = 4 * 3 * 2 * 1
-	 */
-	public static long factorial(int number)
+	public static int factorial(int number)
 	{
-		//base case
-		if (number == 1)
-		{
+		if (number <= 1)
 			return 1;
-		}
 		
-		return number * factorial(number - 1);
-	}
+		return (number * factorial(number - 1));
+	} 
 }
