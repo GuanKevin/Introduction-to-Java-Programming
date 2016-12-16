e95b7d0c21fe162726217e28f1181a353fb32660
Kevin Guan <Kevin Guan>
2016-12-15 20:41:48 -0500

    JavaFX Exercise Delay

--- src/Chapter_17_Binary_IO/Programming_Exercise_09.java
+++ src/Chapter_17_Binary_IO/Programming_Exercise_09.java
@@ -1,5 +1,4 @@
 package Chapter_17_Binary_IO;
-import java.util.Scanner;
 
 /**
  * Address book
@@ -8,14 +7,13 @@ import java.util.Scanner;
  * Use random access file for reading and writing an address. 
  * Assume that the size of name, street, city, state, and zip is 32, 32, 20, 2, 5 bytes, respectively.
  * 
- * 
+ * 12/15/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_09
 {
-	Scanner input = new Scanner(System.in);
 	public static void main(String[] args) 
 	{
 		
