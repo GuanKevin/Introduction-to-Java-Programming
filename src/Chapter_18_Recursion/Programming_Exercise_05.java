47bf981601daa9f4a4385c49abe8d9bd7a35ab95
Kevin Guan <Kevin Guan>
2016-12-17 14:01:25 -0500

    CHP17 PE05 Update: Added print to show series

--- src/Chapter_18_Recursion/Programming_Exercise_05.java
+++ src/Chapter_18_Recursion/Programming_Exercise_05.java
@@ -1,4 +1,5 @@
 package Chapter_18_Recursion;
+
 import java.util.Scanner;
 
 /**
@@ -6,23 +7,33 @@ import java.util.Scanner;
  * Write a recursive method to compute the following series:
  * Write a test program that displays m(i) for i = 1, 2, . . ., 10.
  * 
+ * 12/17/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_05
 {
-	static Scanner input = new Scanner(System.in);
 	public static void main(String[] args) 
 	{
-		System.out.print("Enter i: ");
-		System.out.println(series(input.nextInt()));
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int number = input.nextInt();
+		System.out.print(String.format("%.3f", sumSeries(number)));
+		
+		input.close();
 	}
 	
-	public static double series(double i)
+	public static double sumSeries(int number)
 	{
-		if(i == 1)
-			return (1.0 / 3);
-		return (i / (2.0 * i + 1)) + series(i - 1);
+		if (number <= 1)
+		{
+			System.out.print(number + "/3 = ");
+			return (1.0/3);
+		}
+		
+		System.out.print(number + "/" + ("(2 * ") + number + " + 1)" + " + ");
+		
+		return ((number / ((2.0 * number) + 1)) + sumSeries(--number));
 	}
 }
