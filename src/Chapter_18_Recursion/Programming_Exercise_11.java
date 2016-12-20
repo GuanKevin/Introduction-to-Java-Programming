f54ffbeb1b60347410b6505a7933131db5697b84
Kevin Guan <Kevin Guan>
2016-12-19 19:39:10 -0500

    CHP18 PE11 Updated: sumDigits()

--- src/Chapter_18_Recursion/Programming_Exercise_11.java
+++ src/Chapter_18_Recursion/Programming_Exercise_11.java
@@ -1,4 +1,5 @@
 package Chapter_18_Recursion;
+
 import java.util.Scanner;
 
 /**
@@ -9,33 +10,27 @@ import java.util.Scanner;
  * For example, sumDigits(234) returns 2 + 3 + 4 = 9. 
  * Write a test program that prompts the user to enter an integer and displays its sum.
  * 
- * 06/29/2016
+ * 12/19/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_11
 {
-	static Scanner input = new Scanner(System.in);
 	public static void main(String[] args) 
 	{
+		Scanner input = new Scanner(System.in);
 		System.out.print("Enter a number: ");
 		long number = input.nextLong();
+		System.out.print("The sum of " + number + " is " + sumDigits(number));
 		
-		System.out.print("The sum of the digits " + number + " is "  + sumDigits(number));
+		input.close();
 	}
 	
-	/*
-	 * It takes in a long value and 
-	 * calulate each digit and return the sum
-	 */
 	public static int sumDigits(long n)
 	{
-		if (n == 0)
-		{
+		if ((n / 10) == 0)
 			return (int) n;
-		}
-		
-		return (int) (n % 10 + sumDigits(n / 10));
+		return (int) ((n % 10) + sumDigits(n / 10));
 	}
 }
