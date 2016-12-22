4b025c6d24777de6842bfc5e5074ac4760b3cfb9
Kevin Guan <Kevin Guan>
2016-12-22 18:44:08 -0500

    CHP18 PE22 shorten hexValue()

--- src/Chapter_18_Recursion/Programming_Exercise_22.java
+++ src/Chapter_18_Recursion/Programming_Exercise_22.java
@@ -1,4 +1,5 @@
 package Chapter_18_Recursion;
+
 import java.util.Scanner;
 
 /**
@@ -8,85 +9,44 @@ import java.util.Scanner;
  * public static String dec2Hex(int value) 
  * Write a test program that prompts the user to enter a decimal number and displays its hex equivalent.
  * 
- * 07/05/206
+ * 12/22/206
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_22
 {
-	static Scanner input = new Scanner(System.in);
 	public static void main(String[] args) 
 	{
+		Scanner input = new Scanner(System.in);
 		System.out.print("Enter a decimal: ");
 		int value = input.nextInt();
+		System.out.print("The hexdecimal of " + value + " is " + dec2Hex(value) + ".");
 		
-		System.out.print("The hexadecimal value is " + dec2Hex(value));
+		input.close();
 	}
 	
-	/**
-	 * Uses recursion to return the hexadecimal 
-	 * string from the given decimal value
-	 * 
-	 * @param value
-	 * @return
-	 */
 	public static String dec2Hex(int value) 
 	{
-		String hex = null;
-		
 		if (value < 10)
-		{
-			return Integer.toString(value);
-		}
+			return value + "";
 		else if (value < 16)
-		{
-			return checkHexValue(value);
-		}
-		else
-		{
-			hex = (value % 16 == 0) ? "0" : ((value % 16 < 10) ? Integer.toString(value % 16) : checkHexValue(value % 16));
-		}
+			return hexValue(value);
 		
-		return dec2Hex(value / 16) + hex;
+		return dec2Hex(value / 16) + ((value % 16 < 10) ? (value % 16 + "") : hexValue(value % 16));
 	}
 	
-	/**
-	 * Decimal to Hexadecimal
-	 * 10 -> A
-	 * 11 -> B
-	 * 12 -> C
-	 * 13 -> D
-	 * 14 -> E
-	 * 15 -> F
-	 * @param value
-	 * @return
-	 */
-	public static String checkHexValue(int value)
+	public static String hexValue(int value)
 	{
-		if (value == 10)
+		switch (value)
 		{
-			return "A";
-		}
-		else if (value == 11)
-		{
-			return "B";
-		}
-		else if (value == 12)
-		{
-			return "C";
-		}
-		else if (value == 13)
-		{
-			return "D";
-		}
-		else if (value == 14)
-		{
-			return "E";
-		}
-		else
-		{
-			return "F";
+		case 10: return "A";
+		case 11: return "B";
+		case 12: return "C";
+		case 13: return "D";
+		case 14: return "E";
+		case 15: return "F";
+		default: return "X";
 		}
 	}
 }
