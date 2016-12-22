6588354b48b144fa92d41977f9909c332712290a
Kevin Guan <Kevin Guan>
2016-12-22 16:23:42 -0500

    CHP18 PE16 Added generateList(), updated count()

--- src/Chapter_18_Recursion/Programming_Exercise_16.java
+++ src/Chapter_18_Recursion/Programming_Exercise_16.java
@@ -1,4 +1,7 @@
 package Chapter_18_Recursion;
+
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -8,72 +11,50 @@ import java.util.Scanner;
  * The second one is a recursive helper method.
  * public static int count(char[] chars)
  * public static int count(char[] chars, int high)
- * Write a test program that prompts the user to enter a list of characters in one line and displays the number of uppercase letters in the list.
+ * Write a test program that prompts the user to enter a list of characters in one line and displays the number of 
+ * uppercase letters in the list.
  * 
- * 07/03/2016
+ * 12/22/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_16
 {
-	static Scanner input = new Scanner(System.in);
 	public static void main(String[] args) 
 	{
+		Scanner input = new Scanner(System.in);
 		System.out.print("Enter the size of the array: ");
-		char[] chars = new char[input.nextInt()];
-		inputChar(chars);
-		int high = 0;
-		System.out.print("Uppercase: " + count(chars, high));
-	}
-
-	/*
-	 * Loops through character array index to
-	 * input characters into the array
-	 */
-	public static void inputChar(char[] chars)
-	{
-		for (int i = 0; i < chars.length; i++)
-		{
-			System.out.print("Enter a character: ");
-			chars[i] = input.next().charAt(0);
-		}
+		char[] list = generateList(input.nextInt());
+		System.out.print("Numbers of uppercase in " + Arrays.toString(list) + " is " + count(list) + ".");
+		
+		input.close();
 	}
 	
-	/*
-	 * Counts uppercase characters in the array
-	 */
 	public static int count(char[] chars)
 	{
-		int counter = (chars[0] >= 65 && chars[0] <= 90) ? 1 : 0;
-		
-		if (chars.length == 1)
-		{
-			return counter;
-		}
-		
-		char[] newchars = new char[chars.length - 1];
-		
-		for (int i = 0; i < newchars.length; i++)
-		{
-			newchars[i] = chars[i + 1];
-		}
-		
-		return counter + count(newchars);
+		return count(chars, 0);
 	}
 	
-	/*
-	 * Counts uppercase characters in the array with a helper value
-	 */
 	public static int count(char[] chars, int high)
 	{
-		int counter = (chars[high] >= 65 && chars[high] <= 90) ? 1 : 0;
+		if ((chars.length - 1) == high)
+			return (Character.isUpperCase(chars[high]) ? 1 : 0);
 		
-		if (chars.length - 1 == high)
-		{
-			return counter;
-		}
+		return (Character.isUpperCase(chars[high]) ? 1 : 0) + count(chars, ++high);
+	}
+	
+	public static char[] generateList(int size)
+	{
+		char[] tempList = new char[size];
+		Random randChar = new Random();
 		
-		return counter + count(chars, ++high);
+		for (int i = 0; i < size; i++)
+			if (randChar.nextInt(2) == 0)
+				tempList[i] = (char) (randChar.nextInt(26) + 65);
+			else
+				tempList[i] = (char) (randChar.nextInt(26) + 97);
+		
+		return tempList;
 	}
 }
