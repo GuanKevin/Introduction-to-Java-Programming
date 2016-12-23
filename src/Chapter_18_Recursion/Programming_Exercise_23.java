0adcb05b242e260a3b0f57aa09ba9a8aab0c1c9b
Kevin Guan <Kevin Guan>
2016-12-22 23:04:43 -0500

    Added todo

--- src/Chapter_18_Recursion/Programming_Exercise_23.java
+++ src/Chapter_18_Recursion/Programming_Exercise_23.java
@@ -1,5 +1,4 @@
 package Chapter_18_Recursion;
-import java.util.Scanner;
 
 /**
  * Binary to decimal
@@ -8,36 +7,15 @@ import java.util.Scanner;
  * public static int bin2Dec(String binaryString)
  * Write a test program that prompts the user to enter a binary string and displays its decimal equivalent.
  * 
- * 07/05/2016
+ * 12/22/2016
  * @author kevgu
  *
  */
 
 public class Programming_Exercise_23
 {
-	static Scanner input = new Scanner(System.in);
 	public static void main(String[] args) 
 	{
-		System.out.print("Enter binary string: ");
-		String binaryString = input.next();
-
-		System.out.print("Decimal value is " + bin2Dec(binaryString));
-	}
-	
-	/**
-	 * Uses recursion to find the decimal value of the
-	 * binary string
-	 * 
-	 * @param binaryString
-	 * @return
-	 */
-	public static int bin2Dec(String binaryString)
-	{
-		if (binaryString.length() == 1)
-		{
-			return Character.digit(binaryString.charAt(0), 10);
-		}
 		
-		return (Character.digit(binaryString.charAt(0), 10) * (int) Math.pow(2, binaryString.length() - 1)) + bin2Dec(binaryString.substring(1));
 	}
 }
--- src/Chapter_18_Recursion/Programming_Exercise_29.java
+++ src/Chapter_18_Recursion/Programming_Exercise_29.java
@@ -1,4 +1,6 @@
 package Chapter_18_Recursion;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 /**
@@ -11,9 +13,32 @@ import java.util.Scanner;
 
 public class Programming_Exercise_29
 {
-	Scanner input = new Scanner(System.in);
-	public static void main(String[] args) 
+	public static void main(String[] args) throws FileNotFoundException 
 	{
-		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a directory: ");
+		String path = input.next();
+		System.out.print("Folders in directory " + (new File(path).getAbsolutePath()) + " is " + getNumFiles(new File(path)));
+		input.close();
+	}
+	
+	public static long getNumFiles(File file) throws FileNotFoundException
+	{
+		if (!file.exists()) 
+		     throw new java.io.FileNotFoundException(file + " not found");
+	    if (file.isFile()) 
+	    	return file.length();
+	    else 
+	    {
+	    	File[] files = file.listFiles();
+	    	long size = 0;
+		      
+	    	for (int i = 0; i < files.length; i++) 
+	    	{
+		        size++;
+		        getNumFiles(files[i]);
+	    	}
+	    	return size;
+	    }
 	}
 }
