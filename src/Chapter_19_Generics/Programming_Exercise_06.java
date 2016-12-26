35e0d283a754eceac376acd9ab18add27cd9674e
Kevin Guan <Kevin Guan>
2016-12-26 16:41:54 -0500

    CHP19

--- src/Chapter_19_Generics/Programming_Exercise_06.java
+++ src/Chapter_19_Generics/Programming_Exercise_06.java
@@ -1,11 +1,15 @@
 package Chapter_19_Generics;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Maximum element in a two-dimensional array 
  * Write a generic method that 
  * returns the maximum element in a two-dimensional array. 
  * public static <E extends Comparable<E>> E max(E[][] list)
  * 
+ * 12/
  * @author kevgu
  *
  */
@@ -14,6 +18,35 @@ public class Programming_Exercise_06
 {
 	public static void main(String[] args)
 	{
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the size of the array: ");
+		Integer[][] list = generate2DList(input.nextInt());
+		System.out.print("The largest number in the list is " + max(list));
 		
+		input.close();
+	}
+	
+	public static Integer[][] generate2DList(int size)
+	{
+		Integer[][] tempList = new Integer[size][size];
+		Random randNum = new Random();
+		
+		for (int i = 0; i < size; i++)
+			for (int j = 0; j < size; j++)
+				tempList[i][j] = randNum.nextInt(100);
+		
+		return tempList;
+	}
+	
+	public static <E extends Comparable<E>> E max(E[][] list)
+	{
+		E max = list[0][0];
+		
+		for (int i = 0; i < list.length; i++)
+			for (int j = 0; j < list.length; j++)
+				if (max.compareTo(list[i][j]) == -1)
+					max = list[i][j];
+		
+		return max;
 	}
 }
--- src/Chapter_19_Generics/Programming_Exercise_07.java
+++ src/Chapter_19_Generics/Programming_Exercise_07.java
@@ -1,11 +1,15 @@
 package Chapter_19_Generics;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Generic binary search 
  * Implement the following method using binary search. 
- * public static <E extends Comparable<E>> 
- * int binarySearch(E[] list, E key)
+ * public static <E extends Comparable<E>> int binarySearch(E[] list, E key)
  * 
+ * 12/
  * @author kevgu
  *
  */
@@ -14,6 +18,44 @@ public class Programming_Exercise_07
 {
 	public static void main(String[] args)
 	{
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter size of array: ");
+		Integer[] list = generateArr(input.nextInt());
+		System.out.print("Look for what number: ");
+		int key = input.nextInt();
+		Arrays.sort(list);
+		System.out.println(Arrays.toString(list));
+		System.out.print(key + " is found in index " + binarySearch(list, key));
 		
+		input.close();
+	}
+	
+	public static Integer[] generateArr(int size)
+	{
+		Random randNum = new Random();
+		Integer[] tempList = new Integer[size];
+		
+		for (int i = 0; i < size; i++)
+			tempList[i] = randNum.nextInt(size);
+		
+		return tempList;
+	}
+	
+	public static <E extends Comparable<E>> int binarySearch(E[] list, E key)
+	{
+		return binarySearch(list, key, 0, list.length);
+	}
+	
+	public static <E extends Comparable<E>> int binarySearch(E[] list, E key, int low, int high)
+	{
+		int mid = (low + high) / 2;
+		if (low > high)
+			return -1;
+		else if (list[mid].equals(key))
+			return mid;
+		else if (list[mid].compareTo(key) == -1)
+			return binarySearch(list, key, mid + 1, high);
+		else
+			return binarySearch(list, key, low, mid - 1);
 	}
 }
