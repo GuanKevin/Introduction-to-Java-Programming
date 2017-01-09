4de1d99794bdd39b664fd21b0402934898b2edec
Kevin Guan <Kevin Guan>
2017-01-09 12:04:54 -0500

    Completed CHP22 PE05

--- src/Chapter_22_Developing_Efficient_Algorithms/Programming_Exercise_05.java
+++ src/Chapter_22_Developing_Efficient_Algorithms/Programming_Exercise_05.java
@@ -1,5 +1,9 @@
 package Chapter_22_Developing_Efficient_Algorithms;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Same-number subsequence
  * Write an O(n) program that prompts the user to enter a 
@@ -15,6 +19,57 @@ public class Programming_Exercise_05
 {
 	public static void main(String[] args) 
 	{
+		List<Integer> sequence = getSequence();
+		System.out.println(sequence.toString());
 		
+		System.out.print("The longest sequence is: ");
+		getSubsequence(sequence);
+	}
+	
+	public static void getSubsequence(List<Integer> sequence)
+	{
+		  int index = 0, seqIndex = 0;              
+		  int num, seqNum = sequence.get(0);   
+		  int count = 0, seqCount = 1;
+		  
+		  for (int i = 1; i < sequence.size(); i++) 
+		  {
+			  num = sequence.get(i - 1);
+			  if (sequence.get(i).equals(num)) 
+			  {
+		            if (count == 1) 
+		            	index = i - 1;
+		            
+		            count++;
+		            if (count > seqCount) 
+		            {
+		            	seqCount = count;
+		            	seqNum = num;
+		                seqIndex = index;
+		            }
+		        } 
+			  else 
+		            count = 1;
+		  }
+		
+		System.out.println("Index: " + seqIndex + " Count: " + seqCount + " Num: " + seqNum);
+	}
+	
+	public static ArrayList<Integer> getSequence()
+	{
+		ArrayList<Integer> sequence = new ArrayList<>();
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter sequence of numbers: ");
+		int number = input.nextInt();
+		
+		while (number != 0)
+		{
+			sequence.add(number);
+			number = input.nextInt();
+		}
+		
+		input.close();
+		return sequence;
 	}
 }
