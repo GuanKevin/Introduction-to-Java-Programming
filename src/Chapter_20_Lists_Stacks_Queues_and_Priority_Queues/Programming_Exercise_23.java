01de184b83e1f64f2b06b862c370dd1f3586df7a
Kevin Guan <Kevin Guan>
2017-02-08 12:26:07 -0500

    CHP20 PE23 Incomplete

--- src/Chapter_20_Lists_Stacks_Queues_and_Priority_Queues/Programming_Exercise_23.java
+++ src/Chapter_20_Lists_Stacks_Queues_and_Priority_Queues/Programming_Exercise_23.java
@@ -1,5 +1,48 @@
 package Chapter_20_Lists_Stacks_Queues_and_Priority_Queues;
 
-public class Programming_Exercise_23 {
+import java.util.Scanner;
+import java.util.Stack;
 
+public class Programming_Exercise_23 {
+	public static void main(String [] args)
+	{
+		System.out.print("Enter an expression: ");
+		Scanner scanner = new Scanner(System.in);
+		String expression = scanner.nextLine();
+		System.out.println(expression + " = " + evalExpression(expression));
+	}
+	
+	public static int evalExpression(String expression)
+	{
+		int value = 0;
+		Stack<Integer> stack = new Stack<Integer>();
+		
+		expression.replaceAll("\\s", "");
+		while(expression.contains("^") || expression.contains("*") || expression.contains("/") || 
+		expression.contains("%") || expression.contains("+") || expression.contains("-"))
+		{
+			for(int i = 0; i < expression.length(); i++)
+			{
+				
+				if(expression.charAt(i) == '(')
+					stack.push(i);
+				if(expression.charAt(i) == ')')
+				{
+					if(stack.isEmpty())
+					{
+						System.out.println("Extra parenthesis IDIOT!");
+						System.exit(1);
+					}
+					else
+					{
+						value += evalExpression(expression.substring(stack.peek(), i));
+						stack.pop();
+					}
+				}
+				if(expression.charAt(i) == '^')
+					return 0;
+			}
+		}
+		return value;
+	}
 }
